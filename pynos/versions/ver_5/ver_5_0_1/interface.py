ddd5974f78462c1f088e8820ae0da557e8c890fa
poorani612 <arthanar@brocade.com>
2016-11-18 14:30:12 -0800

    CANDY-120 New pynos calls added

--- pynos/versions/ver_5/ver_5_0_1/interface.py
+++ pynos/versions/ver_5/ver_5_0_1/interface.py
@@ -13,7 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import pynos.utilities
 from pynos.versions.base.interface import Interface as InterfaceBase
+from pynos.versions.ver_6.ver_6_0_1.yang.brocade_mac_address_table \
+    import brocade_mac_address_table
 
 
 class Interface(InterfaceBase):
@@ -24,4 +27,24 @@ class Interface(InterfaceBase):
     Attributes:
         None
     """
-    pass
+
+    def __init__(self, callback):
+        """
+        Interface init function.
+
+        Args:
+            callback: Callback function that will be called for each action.
+
+        Returns:
+            Interface Object
+
+        Raises:
+            None
+        """
+        self._callback = callback
+        self._mac_address_table = brocade_mac_address_table(
+            callback=pynos.utilities.return_xml
+        )
+
+    def mac_move_detect_enable(self, **kwargs):
+        raise NotImplementedError
